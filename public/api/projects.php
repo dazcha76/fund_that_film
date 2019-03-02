1b17604dc7da8ea8e86eb3e9e4ec45f0d372173a
Diana Curtis <dianacurtis92@gmail.com>
2019-03-01 16:04:57 -0800

    API Endpoint Planning Creazted  psudo code for the comparables API endpoint Created psudo code for the projects API endpoint

---
+++ public/api/projects.php
@@ -0,0 +1,15 @@
+ /*
+
+Objective: Insert the data that the user has given us into our database in the projects table.
+
+We expect multiple strings from the client upon accepting the comparable pictures.
+    The strings are title, year, logline, synopsis, production_stage, and genre
+
+We will then sanitize each string and store it in a variable.
+
+We will send the INSERT query to the database seperate from the variables in a prepared statement
+
+We will then INSERT the  strings into the projects table and send a response of TRUE when it is completed
+
+
+ */
