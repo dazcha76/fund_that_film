a33ef1dbe76400712705f1c527fe5b1720b965fb
krispthan <44071872+krispthan@users.noreply.github.com>
2019-03-04 09:58:33 -0800

    updating navbar

---
+++ src/components/aboutus/person.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import scss from '../../section/aboutus.scss';
+
+export default props => {
+    return (
+
+        <div className='card'>
+           <img src={ props.image } className='card-img-top profile-img' alt=''/>
+           <div className='card-body'>
+                <div className='profile-info'>
+                    <div className='profile-name'>
+                        <h5 className='card-title'>{ props.name }</h5>
+                    </div>
+                </div>
+               <div className='social-icon-container'>
+                     <a href={ props.github } ><i className='fab fa-github-square'></i></a>
+                     <a href={ props.linkedin } ><i className='fab fa-linkedin'></i></a>
+                     <a href={ props.portfolio } ><i className='fas fa-briefcase'></i></a>
+               </div>          
+          </div>
+     </div>
+    )
+}
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+// import { BrowserRouter as Router, Route , Link } from 'react-router-dom';
+
 
 export default props => {
     return (
-        <div>
-            <h1>This is the Home</h1>
-        </div>
+            <div>
+                <h1>This is the Home</h1>
+
+               
+            </div>
     )
 }
