7a13587e00903efa16da009215e641f728f69b28
John Holman <j.russell.holman@gmail.com>
2019-03-04 01:48:03 -0800

    Updates to the Projects Endpoint Added more psudo code for what needs to be built out on the endpoint Added a more detailed query to get things started Added error handling Tested in Postman to make sure query was returning properly

--- public/api/projects.php
+++ public/api/projects.php
@@ -1,18 +1,18 @@
 <?php
 
  /*
-
 Objective: Insert the data that the user has given us into our database in the projects table.
 
 We expect multiple strings from the client upon accepting the comparable pictures.
     The strings are title, year, logline, synopsis, production_stage, and genre
+    We will then sanitize each string and store it in a variable.
+    We will send the INSERT query to the database seperate from the variables in a prepared statement
+    We will then INSERT the  strings into the projects table and send a response of TRUE when it is completed
 
-We will then sanitize each string and store it in a variable.
-
-We will send the INSERT query to the database seperate from the variables in a prepared statement
-
-We will then INSERT the  strings into the projects table and send a response of TRUE when it is completed
-
+Next we will make the connections between the project and the comparables in the projects_comparables table
+    we will need to grab the id of the project (server provided)
+    then we will need to grab the ids of the comparable films
+    then we will need to associate them to each other on the projects_comparables table one row for each comparables id
 
  */
 
@@ -23,8 +23,11 @@ $output = [
     'success' => false
 ];
 
-$query = 'SELECT p.`title`, p.`year`, p.`logline`, p.`synopsis`, p.`production_stage`, p.`genre`
-            FROM `projects` AS p';
+$query = 'SELECT p.`id`, p.`title`, p.`year`, p.`logline`, p.`synopsis`, p.`production_stage`, p.`genre`,
+            GROUP_CONCAT(pc.`comparables_id`) AS `comparables_ids`
+            FROM `projects` AS p
+            JOIN `projects_comparables` AS pc ON pc.`projects_id` = p.`id`
+            JOIN `comparables` AS c ON c.`id` = pc.`comparables_id`';
 
 $result = $db -> query($query);
 
