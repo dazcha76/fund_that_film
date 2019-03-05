7e95accc490f32b1581065f454e0f32365797fca
krispthan <44071872+krispthan@users.noreply.github.com>
2019-03-04 23:39:22 -0800

    created details comparisons page with modal

--- src/components/comparables/index.js
+++ src/components/comparables/index.js
@@ -1,11 +1,13 @@
 import comparables from '../../section/comparables.scss';
+import animation from '../../section/animation.scss';
 import React, { Component } from 'react';
 import lakehouse from '../../assets/images/lakehouse.png';
 import spiderman from '../../assets/images/spiderman.png';
-
+import DetailsPage  from './details';
 
 class MovieComparison extends Component {
     state = {
+        active: false,
         movie: [
             {   
                 image: lakehouse,
@@ -20,13 +22,16 @@ class MovieComparison extends Component {
                 roi: '1.5 Million'
             },
         ]
+    } 
+    toggleClass = () => {
+        const currentState = this.state.active;
+        this.setState({active : !currentState});
     }
-    
     buildMovieInfo = (movie) => {
         return (
-            <div className='movies'>
+            <div key = {movie.title} className='movies'>
                 <div className='comparison-movie-display'>
-                    <img src= { lakehouse } id='movie-1-img' className='movie-display'/>
+                    <img src= { movie.image } id='movie-1-img' className='movie-display'/>
                     <div className='movie-title-wrapper'>
                         <h3 className='movie-title-subheader'>{ movie.title } </h3>
                         <h3 className='movie-subheader'>{ movie.dor }</h3>
@@ -34,22 +39,32 @@ class MovieComparison extends Component {
                     </div>
                 </div>
             </div>
-
         ) 
    }
+
    render(){
         const movieInfo = this.state.movie.map(this.buildMovieInfo);
+        const arrowActive = 'is-active';
         return (
-            <div className='comparables-wrapper hidden'>
+            <div>
+            <div className='comparables-wrapper '>
                 <div className='comparables-container'>
                     <div className='header'>
                         <h3 className= 'movie-subtitle'> Movie Comparisons </h3>
-                        { movieInfo }
                         </div>
-                </div>
+                        <div className='movie-info-container'>
+                        { movieInfo }
+                        </div>  
+                        <div onClick = { this.toggleClass } id='arrow-icon'><i className='fas fa-angle-down'></i></div>
+                </div> 
             </div>
+            <div>
+                <DetailsPage detailPageOnclick = {this.state.active} toggleDetailPage = {() => { this.toggleClass()}}/>
+            </div>
+    </div>
         )
    }
 }
 
 export default MovieComparison;
+
