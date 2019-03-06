9a940e46d40b8ae7551497ec1cc436e5437a0001
dazcha76 <dazcha@hotmail.com>
2019-03-05 16:31:14 -0800

    added labels to movie details and formated currency, date and percent values

--- src/components/comparables/details.js
+++ src/components/comparables/details.js
@@ -9,97 +9,97 @@ class DetailsPage extends Component {
             {
                 id:1000,
                 image: spiderman,
-                title: 'Spiderman',
-                genre: 'romantic',
-                theatrical_release: '2018',
-                budget: '934,000',
-                distribution: 'ABC',
-                mpaa: 'r-rated',
-                funding_partners : 'Funding Partners',
-                audience_satisfaction : 'Great',
-                initial_gross: '$323,433',
-                us_gross: '33,234'
+                title: 'The Amazing Spider-Man',
+                genre: 'Adventure',
+                theatrical_release: '2012-07-03',
+                budget: 220000000,
+                distribution: 'Sony Pictures',
+                mpaa: 'PG-13',
+                funding_partners : 'Columbia Pictures, Marvel Studios',
+                audience_satisfaction : 0.77,
+                us_gross: 262030663,
+                intl_gross: 495859604
             },
             {  
                 id:2000,
                 image:lakehouse,
-                title: 'Spiderman',
-                genre: 'romantic',
-                theatrical_release: '2018',
-                budget: '934,000',
-                distribution: 'ABC',
-                mpaa: 'r-rated',
-                funding_partners : 'Funding Partners',
-                audience_satisfaction : 'Great',
-                initial_gross: '$323,433',
-                us_gross: '33,234'
+                title: 'The Lake House',
+                genre: 'Drama',
+                theatrical_release: '2006-06-19',
+                budget: 40000000,
+                distribution: 'Warner Bros.',
+                mpaa: 'PG',
+                funding_partners : 'Touchstone',
+                audience_satisfaction : 0.73,
+                us_gross: 52330111,
+                intl_gross: 62500000
             },
         ]
     }
-        buildMovieDetails = (movieDetails) => {
-            return (
-                <div key={ movieDetails.id } className='modal-movie-img'>
-                            <div className='movie-poster'>
-                                <img className='movie-img-modal' src= { movieDetails.image } />
-                            </div>          
-                        <div className='title-genre-container'>
-                            <div className='title-genre-wrapper'>
-                                <div className='comparison-details'>
-                                    <h2 id='title' className='movie-modal-info'>{ movieDetails.title }</h2>
-                                </div>
-                                <div className='comparison-details'>
-                                        <h2 id='genre' className='movie-modal-info'>{ movieDetails.genre }</h2>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='movie-detail'>
-                                <div className='movie-details-wrapper'>
-                            <div className='comparison-details'>
-                                    <h2 id='budget' className='movie-modal-info'>{ movieDetails.budget }</h2>
-                                </div>
+
+    buildMovieDetails = (movieDetails) => {
+        return (
+            <div key={ movieDetails.id } className='modal-movie-img'>
+                <div className='movie-poster'>
+                    <img className='movie-img-modal' src= { movieDetails.image } />
+                </div>          
+                <div className='title-genre-container'>
+                    <div className='title-genre-wrapper'>
                         <div className='comparison-details'>
-                                <h2 id='mpaa' className='movie-modal-info'>{ movieDetails.mpaa }</h2>
+                            <h2 id='title' className='movie-modal-info'>{ movieDetails.title }</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className='movie-detail'>
+                    <div className='movie-details-wrapper'>
+                        <div className='comparison-details'>
+                            <h2 id='genre' className='movie-modal-info'>Genre: { movieDetails.genre }</h2>
                         </div>
                         <div className='comparison-details'>
-                                <h2 id='us-theatrical' className='movie-modal-info'>{ movieDetails.theatrical_release}</h2>
-                            </div>
-                            <div className='comparison-details'>
-                                    <h2 id='audience-satisfaction' className='movie-modal-info'>{ movieDetails.audience_satisfaction }</h2>
-                                </div>
-                            </div>
-                            <div className='movie-details-wrapper'>
-                                <div className='comparison-details'>
-                                        <h2 id='funding-partner' className='movie-modal-info'>{ movieDetails.funding_partners }</h2>
-                                    </div>
-                                    <div className='comparison-details'>
-                                            <h2 id='initial-gross' className='movie-modal-info'>{ movieDetails.us_gross }</h2>
-                                        </div>
-                                        <div className='comparison-details'>
-                                                <h2 id='distribution' className='movie-modal-info'>{ movieDetails.distribution }</h2>
-                                            </div>
-                                            <div className='comparison-details'>
-                                                    <h2 id='us-gross' className='movie-modal-info'>{ movieDetails.us_gross }</h2>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-            )
-        }
-            render(){
+                            <h2 id='budget' className='movie-modal-info'>Budget: ${ movieDetails.budget.toLocaleString() }</h2>
+                        </div>
+                        <div className='comparison-details'>
+                            <h2 id='mpaa' className='movie-modal-info'>Rating: { movieDetails.mpaa }</h2>
+                        </div>
+                        <div className='comparison-details'>
+                            <h2 id='us-theatrical' className='movie-modal-info'>Release Date: { new Date(movieDetails.theatrical_release).toLocaleDateString('en-US', {day : 'numeric', month : 'long', year : 'numeric'})}</h2>
+                        </div>
+                        <div className='comparison-details'>
+                            <h2 id='audience-satisfaction' className='movie-modal-info'>Audience Satisfaction: { movieDetails.audience_satisfaction * 100}%</h2>
+                        </div>
+                    </div>
+                    <div className='movie-details-wrapper'>
+                        <div className='comparison-details'>
+                            <h2 id='funding-partner' className='movie-modal-info'>Funding Partners: { movieDetails.funding_partners }</h2>
+                        </div>
+                        <div className='comparison-details'>
+                            <h2 id='us-gross' className='movie-modal-info'>U.S. Box Office: ${ movieDetails.us_gross.toLocaleString() }</h2>
+                        </div>
+                        <div className='comparison-details'>
+                            <h2 id='distribution' className='movie-modal-info'>Distribution Comany: { movieDetails.distribution }</h2>
+                        </div>
+                        <div className='comparison-details'>
+                            <h2 id='intl-gross' className='movie-modal-info'>International Box Office: ${ movieDetails.intl_gross.toLocaleString()}</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    render(){
         const individualMovieDetails = this.state.movieDetails.map(this.buildMovieDetails);
         const baseClass = "movie1_comparison_modal";
         console.log();
         return (
- 
             <div className={ this.props.detailPageOnclick ? "active " + baseClass : baseClass }   id='movie_1'>
-            <h1 onClick = {this.props.toggleDetailPage}>X</h1>
-            <div className='modal-content'>
-                { individualMovieDetails }
+                <h1 onClick = {this.props.toggleDetailPage}>X</h1>
+                <div className='modal-content'>
+                    { individualMovieDetails }
+                </div>
             </div>
-         </div>
         )
-     }  
-     
+    }  
 }
 
 export default DetailsPage;
