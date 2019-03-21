6575850bceda6d5a28b31d6e4d703441aef3d375
Diana Curtis <dianacurtis92@gmail.com>
2019-03-20 17:41:53 -0700

    tested the query through phpmyAdmin to select the user id gave output to frontend to say whether or not the user is loged in did error handling for making sure that only one user should be comming back

--- public/api/sign_in.php
+++ public/api/sign_in.php
@@ -5,42 +5,63 @@ require_once('../../config/setup.php');
 require_once('../../config/mysqlconnect.php');
 
 $output = [
-    'sucess'=>false
+    'success'=>false,
+    'login'=>false
 ];
 
+    $data = json_decode( file_get_contents( 'php://input'),true);
+
+    if(!$data){
+        throw new Exception('no data was sent');
+    }
+
+    $login=$data['login'];
+
+    if(empty($login['email'])){
+        throw new Exception('email is a required field');
+    }
+
+    if(!filter_var($login['email'], FILTER_VALIDATE_EMAIL)){
+        throw new Exception('Not a valid email');
+    }
+
+    if(empty($login['password'])){
+        throw new Exception('password is a required field');
+    }
+
+    $password = sha1($data['login']['password']);
+    unset($data['login']['password']);
+
+    $query ="SELECT `id` 
+                FROM  `users` AS u
+                WHERE  u.`password`='{$password}'
+                AND u.`email`= '{$login['email']}' ";
 
 
-$data = json_decode( file_get_contents( 'php://input'),true);
+    $result = $db->query($query);
 
-if(!$data){
-    throw new Exception('no data was sent');
-}
+    if($result){
+        if($result->num_rows===1){
+            while($row=$result->fetch_assoc()){
+                $user_id=$row['id'];
+            }
 
+            $_SESSION['user_id']=$user_id;
+            $output['success']=true;
+            $output['login']=true;
 
-$password = sha1($data['login']['password']);
-$login=$data['login'];
+        }else{
+            throw new Exception('Invalid email or password');
+        } 
+    }else{
+        throw new Exception('SQL Error');
+    }
 
-if(empty($login['email'])){
-    throw new Exception('email is a required field');
-}
-
-if(!filter_var($login['email'], FILTER_VALIDATE_EMAIL)){
-    throw new Exception('Not a valid email');
-}
-
-if(empty($login['password'])){
-    throw new Exception('password is a required field');
-}
-
-
+    $json_output=json_encode($output);
+    print_r($json_output);
 
 
 
-
-
-$query ='SELECT `id` 
-            FROM  `users` AS u
-            WHERE u.`email`= '{$login['email']}'  ';
 
 
 ?>
