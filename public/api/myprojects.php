ad3ec8de3de90a822ce3ed783e0130f968e34e3d
Diana Curtis <dianacurtis92@gmail.com>
2019-03-22 18:12:51 -0700

    built out the query to use a number from sessions able to get all the ids from the users projects table now need to build out another query in order grab all the projects from the database

--- public/api/myprojects.php
+++ public/api/myprojects.php
@@ -9,12 +9,21 @@ $output=[
 ];
 
 
-// for the user id we want to grab the projects id and match it to the project of the user
-
-
-$query ="SELECT `projects_id`
+$id_query ="SELECT `projects_id`
             FROM `users_projects`
-            WHERE `users_id`='{$_SESSION['user_id']}'";
-print_r($query);
+            WHERE `users_id`={$_SESSION['user_id']}";
+
+
+
+$id_result = $db->query($id_query);
+$id_output=[];
+
+while($row=$id_result->fetch_assoc()){
+    $id_output[]=$row['projects_id'];
+};
+
+print_r($id_output);
+
+
 
 ?>
