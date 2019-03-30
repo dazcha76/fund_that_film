59e03b44edba4182b505cad5f78119c640eca2f5
John Holman <j.russell.holman@gmail.com>
2019-03-30 04:03:44 -0700

    Added the ability to add two titles that are currently not in the database

--- public/api/comparables.php
+++ public/api/comparables.php
@@ -148,7 +148,7 @@ if($id_result -> num_rows === 1){
                         }
                     } else {
                         $insert_funding_query = "INSERT INTO `funding_partners` (`name`)
-                                                    VALUES ".$movies_detail_data['production_companies'][$fundingIndex]['name'].";";
+                                                    VALUES ('".$movies_detail_data['production_companies'][$fundingIndex]['name']."');";
                         
                         $insert_funding_result = $db -> query($insert_funding_query);
                         
@@ -214,6 +214,152 @@ if($id_result -> num_rows === 1){
         }
     } else {
         throw new Exception('There was no response from the API.');
+    }
+} elseif ($id_result -> num_rows === 0){
+    $newTitles = [$_GET['title1'],$_GET['title2']];
+    for($newTitleIndex = 0; $newTitleIndex < count($newTitles); $newTitleIndex++){
+        $movies_url = 'https://api.themoviedb.org/3/search/movie?api_key='.urlencode($movie_key).'&query='.urlencode($newTitles[$newTitleIndex]);
+        $movies_title = file_get_contents($movies_url);
+        $movies_title_data = json_decode($movies_title, true);
+
+        if(count($movies_title_data['results']) > 0){
+            for($titleIndex=0; $titleIndex < count($movies_title_data['results']); $titleIndex++){
+                if($movies_title_data['results'][$titleIndex]['title'] === $bodyVars['title1'] || $movies_title_data['results'][$titleIndex]['title'] === $bodyVars['title2']){
+                    $movies_id = 'https://api.themoviedb.org/3/movie/'.intval($movies_title_data['results'][$titleIndex]['id']).'?api_key='.urlencode($movie_key);
+                    $movies_detail = file_get_contents($movies_id);
+                    $movies_detail_data = json_decode($movies_detail, true);
+                    
+                    $movies_mpaa_rating = 'https://api.themoviedb.org/3/movie/'.intval($movies_title_data['results'][$titleIndex]['id']).'/release_dates?api_key='.urlencode($movie_key);
+                    $movies_mpaa_detail = file_get_contents($movies_mpaa_rating);
+                    $movies_mpaa_data = json_decode($movies_mpaa_detail, true);
+                    
+                    $mpaa_certification = '';
+                    if(count($movies_mpaa_data['results']) > 0){
+                        for($mpaaIndex=0; $mpaaIndex < count($movies_mpaa_data['results']); $mpaaIndex++){
+                            if($movies_mpaa_data['results'][$mpaaIndex]['iso_3166_1'] === 'US'){
+                                $mpaa_certification = $movies_mpaa_data['results'][$mpaaIndex]['release_dates'][0]['certification'];
+                            }
+                        }
+                    }
+            
+                    $insert_us_gross_bo = floor($movies_detail_data["revenue"] * .6);
+                    $insert_intl_gross_bo = floor($movies_detail_data["revenue"] * .4);
+                    $insert_audience_satisfaction = $movies_detail_data["vote_average"] / 10;
+                    $Date = $movies_detail_data["release_date"];
+                    $insert_us_theatrical_end = date('Y-m-d', strtotime($Date. ' + 90 days'));
+                    
+                    $insert_comp_query = "INSERT INTO `comparables` SET `title`= '{$movies_detail_data["title"]}', 
+                        `us_theatrical_release`= '{$movies_detail_data["release_date"]}', 
+                        `us_gross_bo`= '{$insert_us_gross_bo}', 
+                        `intl_gross_bo`= '{$insert_intl_gross_bo}',
+                        `budget`= '{$movies_detail_data["budget"]}',
+                        `mpaa_rating`= '{$mpaa_certification}',
+                        `audience_satisfaction`= '{$insert_audience_satisfaction}',
+                        `us_theatrical_end`= '{$insert_us_theatrical_end}',
+                        `genre`= '{$movies_detail_data["genres"][0]["name"]}'";
+                    
+                    $insert_comp_result = $db -> query($insert_comp_query);
+                    
+                    if($insert_comp_result){
+                        $insert_comp_id = mysqli_insert_id($db);
+                    } else {
+                        throw new Exception('There was an error with the film that you are trying to enter.');
+                    }
+            
+                    $insert_poster_query = "INSERT INTO `comparables_images` 
+                                                SET `comparables_id`= '{$insert_comp_id}', 
+                                                `image_url`= 'https://image.tmdb.org/t/p/w600_and_h900_bestv2{$movies_detail_data["poster_path"]}'";
+            
+                    $insert_poster_result = $db -> query($insert_poster_query);
+                    
+                    if($insert_poster_result){
+                        $insert_poster_id = mysqli_insert_id($db);
+                    } else {
+                        throw new Exception('The poster you were trying to add was not added.');
+                    }
+            
+                    $funding_id_array = [];
+            
+                    for($fundingIndex=0; $fundingIndex < count($movies_detail_data['production_companies']); $fundingIndex++){
+                        $select_funding_query = "SELECT fp.`id`, fp.`name`
+                                                    FROM `funding_partners` AS fp
+                                                    WHERE fp.`name` = '".$movies_detail_data['production_companies'][$fundingIndex]['name']."'";
+            
+                        $select_funding_result = $db -> query($select_funding_query);
+            
+                        if($select_funding_result -> num_rows === 1){
+                            while($row_id = $select_funding_result -> fetch_assoc()){
+                                $funding_id_array[] = $row_id['id'];
+                            }
+                        } else {
+                            $insert_funding_query = "INSERT INTO `funding_partners` (`name`)
+                                                        VALUES ('".$movies_detail_data['production_companies'][$fundingIndex]['name']."');";
+                            
+                            $insert_funding_result = $db -> query($insert_funding_query);
+                            
+                            if($insert_funding_result){
+                                $funding_id_array[] = mysqli_insert_id($db);
+                            } else {
+                                throw new Exception('The funding partner you were trying to add was not added.');
+                            }    
+                        }
+                    }
+            
+                    $compFundPiece='';
+            
+                    for($compFundIndex=0; $compFundIndex < count($funding_id_array); $compFundIndex++){
+                        $compFundPiece.= '('.$insert_comp_id.', '.$funding_id_array[$compFundIndex].')';
+                        if($compFundIndex < count($funding_id_array) - 1){
+                            $compFundPiece.= ', ';
+                        }
+                    }
+            
+                    $insert_comp_fund_query = "INSERT INTO `comparables_funding` (`comparables_id`, `funding_partners_id`)
+                                                VALUES ".$compFundPiece.";";
+            
+                    $insert_comp_fund_result = $db -> query($insert_comp_fund_query);
+                    
+                    $distribution_title = '';
+            
+                    $distribution_url = 'http://www.omdbapi.com/?i='.$movies_detail_data['imdb_id'].'&apikey='.$open_key;
+                    $distribution_company_name = file_get_contents($distribution_url);
+                    $distribution_company_data = json_decode($distribution_company_name, true);
+                    $distribution_title = $distribution_company_data['Production'];
+            
+                    $select_distribution_query = "SELECT dc.`id`, dc.`name`
+                                                    FROM `distribution_companies` AS dc
+                                                    WHERE dc.`name` = '{$distribution_title}'";
+                    
+                    $select_distribution_query_result = $db -> query($select_distribution_query);
+                    
+                    if($select_distribution_query_result -> num_rows === 1){
+                        while($row_id = $select_distribution_query_result -> fetch_assoc()){
+                            $insert_distribution_id = $row_id['id'];
+                        }
+                    } else {
+                        $insert_distribution_query = "INSERT INTO `distribution_companies` 
+                                                        SET `name`= '{$distribution_title}'";
+                        
+                        $insert_distribution_result = $db -> query($insert_distribution_query);
+                        
+                        if($insert_distribution_result){
+                            $insert_distribution_id = mysqli_insert_id($db);
+                        } else {
+                            throw new Exception('The distribution company you were trying to add was not added.');
+                        }
+                    }
+                    
+                    $insert_comp_dist_query = "INSERT INTO `comparables_distribution` (`comparables_id`, `distribution_companies_id`) 
+                                                VALUES (".$insert_comp_id.", ".$insert_distribution_id.");";
+                
+                    $insert_comp_dist_result = $db -> query($insert_comp_dist_query);
+            
+                    $id_array[] = $insert_comp_id;
+                }
+            }
+        } else {
+            throw new Exception('There was no response from the API.');
+        }
     }
 }
 
