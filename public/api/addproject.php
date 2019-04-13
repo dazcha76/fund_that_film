bf66ab2ef2d80a5b5df3225f862d037ac0078884
Diana Curtis <dianacurtis92@gmail.com>
2019-04-12 18:28:55 -0700

    build the query to insert the data for the new user the query works through phpmyadmin but does not through postman

--- public/api/addproject.php
+++ public/api/addproject.php
@@ -6,18 +6,7 @@ $output = [
     'success' => false
 ];
 $incoming_request = json_decode( file_get_contents( 'php://input'),true);
-// $request = [
-//     "runtime"=> "130",
-//     "logline"=> "In class no one can hear you scream",
-//     "title"=> "The Greatest Movie",
-//     "releasedYear"=> "2019",
-//     "genre"=> "Horror",
-//     "mpaa"=> "PG-13",
-//     "developmentStage"=> "Pre-production",
-//     "synopsis"=> "Student try to finalize a student project as well as juggling the demands of a portfolio and trying to find a job.",
-//     "film1"=> "The Amazing Spider-Man",
-//     "film2"=> "The Lake House"
-// ];
+
 
 $required_keys=['runtime','logline','title','releasedYear','genre','mpaa','developmentStage','synopsis','film1','film2'];
 $input_keys_array=[];
--- public/api/signup.php
+++ public/api/signup.php
@@ -8,13 +8,39 @@ require_once('../../config/mysqlconnect.php');
 
 $output = [
     'success'=> false
-
 ];
+
+$data = json_decode( file_get_contents( 'php://input'),true);
+
+if(!$data){
+    throw new Exception('No data was sent');
+}
+
+$email = $data["email"];
+$password = sha1($data["password"]);
+$dateJoined = date("y-m-d h:i:s");
+unset($data["password"]);
+
+$query_insert_user = "INSERT INTO `users` SET `name` = {$data["name"]},
+            `last_login` = {$dateJoined},
+        `email` = {$email},
+        `password` = {$password} ";
+
+
+$result_insert_user = $db ->query($query_insert_user);
+
+$user_id = mysqli_insert_id($db);
+print_r($user_id);
+
+
+
+
+
+
+
 
 $json_output = json_encode($output);
 print_r($json_output);
-
-
 
 
 ?>
